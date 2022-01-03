3cd71e9d42b29b49c8f720a31e4dbe2fcdc718b0
WMex <52628801+wmex91@users.noreply.github.com>
2022-01-03 15:58:36 +0100

    Project file import
    Project file import from v2 project.

---
+++ machine_model.py
@@ -0,0 +1,178 @@
+"""
+Machine_A.py class:
+
+Class describing a single machine behaviour.
+Random breakdowns are embedded in the model.
+The file have to be integrated with the env.
+
+It takes as input also the LogisticModel, to get and put raw materials from warehouses.
+"""
+import random
+import simpy
+from GlobalVariables import *
+import Monitoring
+
+
+# TODO: Add the breakdown logging and console printing. Test the breakdown system.
+# MACHINE CLASS --------------------------------------------------------------------------------------------------------
+class Machine(object):
+    """A machine produces parts and gets broken every now and then.
+
+    If it breaks, it requires a "repairman" and continues
+    the production when is repaired.
+
+    A machine has a "name" and a number of parts processed.
+    """
+    def __init__(self, env, name, mean_process_time, sigma_process_time, MTTF, repair_time, input_buffer,
+                 output_buffer):
+        self.env = env
+        self.name = name
+
+        # Process variables.
+        self.parts_made = 0
+        self.mean_process_time = mean_process_time
+        self.sigma_process_time = sigma_process_time
+
+        # Breakdowns variables.
+        self.MTTF = MTTF
+        self.break_mean = 1 / self.MTTF
+        self.repair_time = repair_time
+        self.broken = False
+
+        self.input_buffer = input_buffer
+        self.output_buffer = output_buffer
+
+        # Start "working" and "break_machine" processes for this machine.
+        self.process = env.process(self.working())
+        self.env.process(self.break_machine())  # To exclude breakdowns, comment this line.
+
+        # Logging objects
+        self.log_path = GlobalVariables.LOG_PATH_generic_version
+        self.log_filename = GlobalVariables.LOG_FILENAME
+        self.data_logger = Monitoring.DataLogger(self.log_path, self.log_filename)
+        self.data_logger.write_log("### DATA LOG FROM PROCESS MACHINE FILE ###\n")
+
+    #  Function describing the machine process.
+    def working(self):
+        """Produces parts as long as the simulation runs.
+
+        While making a part, the machine may break multiple times.
+        Request a repairman when this happens.
+        """
+        while True:
+            # Perform the output warehouse level checking: if empty, wait 1 time step. NOT WORKING.
+            input_node_level = self.input_buffer.input_container.level
+            """
+            while input_node_level == 0:
+                print(str(self.env.now) + ": the " + self.name + " input buffer level is " + str(input_node_level) +
+                      ". Waiting 1 time step and re-check.")
+                self.data_logger.write_log(str(self.env.now) + ": the " + self.name + " input buffer level is "
+                                           + str(input_node_level) + ". Waiting 1 time step and re-check.\n")
+                yield self.env.timeout(1)
+            """
+            # Warehouse tracking made before the piece to be took/placed.
+            input_time = self.env.now
+
+            # Take the raw product from raw products warehouse
+            self.input_buffer.input_container.get(1)
+
+            # Start making a new part
+            time_per_part = self.mean_process_time
+            done_in = time_per_part       # time_per_part() is the real stochastic function.
+            while done_in:
+                try:
+                    # Working on the part
+                    start = self.env.now
+                    yield self.env.timeout(done_in)
+                    done_in = 0     # Set 0 to exit to the loop
+
+                except simpy.Interrupt:
+                    # The machine broke.
+                    self.broken = True
+                    done_in -= self.env.now - start     # How much time left to finish the job?
+
+                    # Logging the event.
+                    print("\n" + str(self.env.now) + ": " + self.name + " broke." + str(done_in) +
+                          " step for the job to be completed. Machine will be repaired in " +
+                          str(self.repair_time) + "\n")
+                    self.data_logger.write_log(str(self.env.now) + ": " + self.name + " broke. " + str(done_in) +
+                                               " step for the job to be completed.")
+                    self.data_logger.write_log("Machine will be repaired in " + str(self.repair_time) + "\n")
+
+                    yield self.env.timeout(self.repair_time)
+
+                    # Machine repaired.
+                    self.broken = False
+
+                    # Logging the event. The machine is repaired.
+                    print(str(self.env.now) + ": " + self.name + " repaired. Working restarted.")
+                    self.data_logger.write_log(str(self.env.now) + ": " + self.name +
+                                               " repaired. Working restarted.\n")
+
+            # Part is done
+            prod_time = self.env.now
+            self.parts_made += 1
+            print(str(prod_time) + ": Node " + self.name + " assembled {0} pieces.".format(self.parts_made))
+
+            # Perform the output warehouse level checking: if full, wait 1 time step. NOT WORKING
+            output_node_level = self.output_buffer.output_container.level
+            """
+            while output_node_level == self.output_buffer.output_container.capacity:
+                print(str(self.env.now) + ": the " + self.name + " output buffer level is " + str(output_node_level) +
+                      ". Waiting 1 time step and re-check.")
+                self.data_logger.write_log(str(self.env.now) + ": the " + self.name + " output buffer level is " +
+                                           str(output_node_level) + ". Waiting 1 time step and re-check.\n")
+                yield self.env.timeout(1)
+            """
+            # Put the finished product into the finished warehouse
+            self.output_buffer.output_container.put(1)
+            output_time = self.env.now
+
+            # Writing to the log file
+            self.data_logger.write_log(str(input_time) + ".1: input " + self.name + " level " + str(input_node_level)
+                                       + "; get 1 from input " + self.name + ".\n")
+            self.data_logger.write_log(str(prod_time) + ".2: made 1 in " + self.name + ".\n")
+            self.data_logger.write_log(str(output_time) + ".3: output " + self.name + " level " + str(output_node_level)
+                                       + "; put 1 in output " + self.name + ".\n")
+        # yield self.env.timeout(0)
+
+    def break_machine(self):
+        """Break the machine every now and then."""
+        while True:
+            time_to_failure = round(random.expovariate(self.break_mean))
+            yield self.env.timeout(time_to_failure)
+            if not self.broken:
+                # Only breaks when machine is currently working.
+                self.process.interrupt()
+
+
+"""
+# REPAIRMAN FUNCTION ---------------------------------------------------------------------------------------------------
+def other_jobs(env, repairman, other_jobs_time):
+    # Those are repairman's other not important jobs.
+    while True:
+        # Start a new job
+        done_in = other_jobs_time
+        while done_in:
+            # Retry the job until is done
+            # Its priority is lower than that of machine repairs
+            with repairman.request(priority=2) as req:
+                yield req
+                try:
+                    start = env.now
+                    yield env.timeout(done_in)
+                    done_in = 0
+                except simpy.Interrupt:
+                    done_in -= env.now - start
+"""
+
+# MINOR FUNCTIONS ------------------------------------------------------------------------------------------------------
+"""
+def time_per_part():
+    Return actual processing time for a concrete part.
+    return random.normalvariate(PT_MEAN, PT_SIGMA)
+
+def time_to_failure():
+    Return time until next failure for a machine
+    return random.expovariate(BREAK_MEAN)
+"""
---
+++ running_model.py
@@ -0,0 +1,93 @@
+"""
+Model.py file:
+
+that's the class where the model is described and launched.
+"""
+
+# TODO: refactor the import statements adding the package reference in each imported file.
+from LogisticModel_Generic import *
+from Machine_Generic import *
+
+
+# ENVIRONMENT DEFINITION -----------------------------------------------------------------------------------------------
+env = simpy.Environment()
+
+# LOGISTIC ENTITIES DEFINITION -----------------------------------------------------------------------------------------
+input_A = LogisticWrapper(env, max_capacity=GlobalVariables.CONTAINER_A_RAW_CAPACITY,
+                          init_capacity=GlobalVariables.INITIAL_A_RAW, bool_input_control_container=True,
+                          critical_level_input_container=GlobalVariables.CRITICAL_STOCK_A_RAW,
+                          supplier_lead_time=GlobalVariables.SUPPLIER_LEAD_TIME_A_RAW,
+                          supplier_std_supply=GlobalVariables.SUPPLIER_STD_SUPPLY_A_RAW,
+                          input_refilled_check_time=GlobalVariables.AFTER_REFILLING_CHECK_TIME_A_RAW,
+                          input_std_check_time=GlobalVariables.STANDARD_A_CHECK_TIME,
+                          bool_output_control_container=False)
+
+output_A = LogisticWrapper(env, max_capacity=GlobalVariables.CONTAINER_A_FINISHED_CAPACITY,
+                           init_capacity=GlobalVariables.INITIAL_A_FINISHED, bool_input_control_container=False,
+                           bool_output_control_container=False)
+
+input_B = LogisticWrapper(env, max_capacity=GlobalVariables.CONTAINER_B_RAW_CAPACITY,
+                          init_capacity=GlobalVariables.INITIAL_B_RAW, bool_input_control_container=True,
+                          critical_level_input_container=GlobalVariables.CRITICAL_STOCK_B_RAW,
+                          supplier_lead_time=GlobalVariables.SUPPLIER_LEAD_TIME_B_RAW,
+                          supplier_std_supply=GlobalVariables.SUPPLIER_STD_SUPPLY_B_RAW,
+                          input_refilled_check_time=GlobalVariables.AFTER_REFILLING_CHECK_TIME_B_RAW,
+                          input_std_check_time=GlobalVariables.STANDARD_B_CHECK_TIME,
+                          bool_output_control_container=False)
+
+output_B = LogisticWrapper(env, max_capacity=GlobalVariables.CONTAINER_B_FINISHED_CAPACITY,
+                           init_capacity=GlobalVariables.INITIAL_B_FINISHED, bool_input_control_container=False,
+                           bool_output_control_container=False)
+
+input_C = LogisticWrapper(env, max_capacity=GlobalVariables.CONTAINER_C_FINISHED_CAPACITY,
+                          init_capacity=GlobalVariables.INITIAL_C_FINISHED, bool_input_control_container=False,
+                          bool_output_control_container=False)
+
+output_C = LogisticWrapper(env, max_capacity=GlobalVariables.CONTAINER_C_FINISHED_CAPACITY,
+                           init_capacity=GlobalVariables.INITIAL_C_FINISHED, bool_input_control_container=False,
+                           bool_output_control_container=True,
+                           critical_level_output_container=GlobalVariables.CRITICAL_STOCK_C_FINISHED,
+                           dispatcher_lead_time=GlobalVariables.DISPATCHER_LEAD_TIME_C_FINISHED,
+                           dispatcher_retrieved_check_time=GlobalVariables.DISPATCHER_RETRIEVED_CHECK_TIME_C_FINISHED,
+                           dispatcher_std_check_time=GlobalVariables.DISPATCHER_STD_CHECK_TIME_C_FINISHED)
+
+# REPAIRMEN DEFINITION -------------------------------------------------------------------------------------------------
+# TODO: after the process testing, switch from 3 to 1 repairman definition, changing its capacity.
+# actual_repairman_A = simpy.PreemptiveResource(env, capacity=1)
+# actual_repairman_B = simpy.PreemptiveResource(env, capacity=1)
+# actual_repairman_C = simpy.PreemptiveResource(env, capacity=1)
+
+# MACHINES DEFINITION --------------------------------------------------------------------------------------------------
+machine_A = Machine(env, "Machine A", GlobalVariables.MEAN_PROCESS_TIME_A, GlobalVariables.SIGMA_PROCESS_TIME_A,
+                    GlobalVariables.MTTF_A, GlobalVariables.REPAIR_TIME_A, input_A, output_A)
+machine_B = Machine(env, "Machine B", GlobalVariables.MEAN_PROCESS_TIME_B, GlobalVariables.SIGMA_PROCESS_TIME_B,
+                    GlobalVariables.MTTF_B, GlobalVariables.REPAIR_TIME_B, input_B, output_B)
+
+# TODO: maybe **args and **kwargs could help here?
+# Moving from output A&B to input C
+output_A.input_container.get(1)
+output_B.input_container.get(1)
+input_C.input_container.put(1)
+
+machine_C = Machine(env, "Machine C", GlobalVariables.MEAN_PROCESS_TIME_C, GlobalVariables.SIGMA_PROCESS_TIME_C,
+                    GlobalVariables.MTTF_C, GlobalVariables.REPAIR_TIME_C, input_C, output_C)
+
+# SIMULATION RUN! ------------------------------------------------------------------------------------------------------
+print(f'STARTING SIMULATION')
+print(f'----------------------------------')
+
+env.run(until=int(GlobalVariables.SIM_TIME))
+
+print('Node A raw container has {0} pieces ready to be processed'.format(input_A.input_container.level))
+print('Node A finished container has {0} pieces ready to be processed'.
+      format(output_A.output_container.level))
+
+print('Node B raw container has {0} pieces ready to be processed'.format(input_B.input_container.level))
+print('Node B finished container has {0} pieces ready to be processed'.format(output_B.output_container.level))
+
+print(f'Dispatch C has %d pieces ready to go!' % output_C.output_container.level)
+print(f'----------------------------------')
+print('total pieces delivered: {0}'.format(output_C.products_delivered + output_C.output_container.level))
+print('total pieces assembled: {0}'.format(machine_C.parts_made))
+print(f'----------------------------------')
+print(f'SIMULATION COMPLETED')

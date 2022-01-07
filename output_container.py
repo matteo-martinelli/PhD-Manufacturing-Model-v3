8beb4a67be9e79a3982d2a99ee71ca00652a20c5
WMex <52628801+wmex91@users.noreply.github.com>
2022-01-07 16:11:23 +0100

    Minor changes
    Minor changes.

--- output_container.py
+++ output_container.py
@@ -37,7 +37,7 @@ class OutputContainer(simpy.Container):
         self.log_path = GlobalVariables.LOG_PATH
         self.log_filename = GlobalVariables.LOG_FILENAME
         self.data_logger = DataLogger(self.log_path, self.log_filename)
-        self.data_logger.write_log("### DATA LOG FROM PROCESS MACHINE FILE ###\n")
+        self.data_logger.write_log("### DATA LOG FROM OUTPUT CONTAINER FILE ###\n")
 
     def output_control_container(self, env):
         yield env.timeout(0)

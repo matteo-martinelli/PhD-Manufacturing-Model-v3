088bc3ce6c39acd3accfe4861f7305cfbd215a8f
WMex <52628801+wmex91@users.noreply.github.com>
2022-01-12 09:57:52 +0100

    Update monitoring.py
    Updated with Excel log file initialization.

--- monitoring.py
+++ monitoring.py
@@ -33,38 +33,44 @@ class DataLogger(object):
             os.remove(self.complete_filename_txt)
         except FileNotFoundError:
             print("The log file has not been found in the directory, creating a new one.")
-            os.makedirs(self.path)
+            # os.makedirs(self.path)
             with open(self.complete_filename_txt, "w") as f:
                 f.close()
 
     def initialize_excel_file(self):
-        # Create the workbook
-        wb = Workbook()
-        # add_sheet is used to create sheet.
-        sheet1 = wb.active
-        sheet1.title = "Sheet 1"
-        # Writing the heading
-        sheet1['A1'] = "Timestep [step]"
+        try:
+            os.remove(self.complete_filename_xlsx)
+        except FileNotFoundError:
+            print("The excel file has not been found in the directory, creating a new one.")
+            # os.makedirs(self.path)
 
-        sheet1['B1'] = "input A [level]"
-        sheet1['C1'] = "timeprocess A [step]"
-        sheet1['D1'] = "output A [level]"
-        sheet1['E1'] = "failure A [bool]"
-        sheet1['F1'] = "MTTF A [step]"
-        sheet1['G1'] = "MTTR A [step]"
+            # Create the workbook
+            wb = Workbook()
+            # add_sheet is used to create sheet.
+            sheet1 = wb.active
+            sheet1.title = "Sheet 1"
+            # Writing the heading
+            sheet1['A1'] = "Timestep [step]"
 
-        sheet1['H1'] = "input B [level]"
-        sheet1['I1'] = "timeprocess B [step]"
-        sheet1['J1'] = "output B [level]"
-        sheet1['K1'] = "failure B [bool]"
-        sheet1['L1'] = "MTTF B [step]"
-        sheet1['M1'] = "MTTR B [step]"
+            sheet1['B1'] = "input A [level]"
+            sheet1['C1'] = "timeprocess A [step]"
+            sheet1['D1'] = "output A [level]"
+            sheet1['E1'] = "failure A [bool]"
+            sheet1['F1'] = "MTTF A [step]"
+            sheet1['G1'] = "MTTR A [step]"
 
-        sheet1['N1'] = "input C [level]"
-        sheet1['O1'] = "timeprocess C [step]"
-        sheet1['P1'] = "output C [level]"
-        sheet1['Q1'] = "failure C [bool]"
-        sheet1['R1'] = "MTTF C [step]"
-        sheet1['S1'] = "MTTR C [step]"
+            sheet1['H1'] = "input B [level]"
+            sheet1['I1'] = "timeprocess B [step]"
+            sheet1['J1'] = "output B [level]"
+            sheet1['K1'] = "failure B [bool]"
+            sheet1['L1'] = "MTTF B [step]"
+            sheet1['M1'] = "MTTR B [step]"
 
-        wb.save(self.complete_filename_xlsx)
+            sheet1['N1'] = "input C [level]"
+            sheet1['O1'] = "timeprocess C [step]"
+            sheet1['P1'] = "output C [level]"
+            sheet1['Q1'] = "failure C [bool]"
+            sheet1['R1'] = "MTTF C [step]"
+            sheet1['S1'] = "MTTR C [step]"
+
+            wb.save(self.complete_filename_xlsx)

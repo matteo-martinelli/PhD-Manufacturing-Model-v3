dd3421903a2347b4c12309832f957be0d1d3edbe
WMex <52628801+wmex91@users.noreply.github.com>
2022-01-17 17:30:46 +0100

    Update log_merger.py
    Merging logs logic implementation.

--- log_merger.py
+++ log_merger.py
@@ -6,6 +6,7 @@ The merged file has to be consistent with time order.
 """
 
 import os
+from global_variables import *
 
 
 class LogMerger(object):
@@ -34,7 +35,6 @@ class LogMerger(object):
                 # Write the head in the merged file.
                 # If is the first file, add the step column at the file beginning.
                 if args.index(arg) == 0:
-                    print("there1")
                     with open(self.save_path + "\\merged_logs.txt", "a") as output_file:
                         output_file.write(line[:-1])
                         output_file.close()
@@ -53,4 +53,46 @@ class LogMerger(object):
 
         print("Headings merged.")
 
+        # Populating the time step column in the txt file. Every simulation time step will be displayed.
+        with open(self.save_path + "\\merged_logs.txt", "a") as output_file:
+            output_file.write("\n")
+            output_file.write("-1, \n")
+            for i in range(GlobalVariables.SIM_TIME):
+                output_file.write(str(i) + ", \n")
+            output_file.close()
 
+        # Populating the merged_logs.txt file with the merged heading.
+        sel = 0
+        for arg in args:
+            # Open the machine log file.
+            with open(self.save_path + "\\" + arg, "r") as input_file:
+                # The selector will be multiplied with the column size counter
+                # TODO: implement the column size counter in the DataLogger class.
+                sel += 1
+                while True:
+                    # Read the i-th line of the machine log.
+                    input_line = input_file.read().split(", ")
+                    input_step = input_line[0]
+                    # Skip the first line, the heading one.
+                    if input_step == "step":
+                        print("Ignoring the head. Skipping to the next line")
+                        continue
+                    else:
+                        # Open the merged_log.txt file.
+                        with open(self.save_path + "\\merged_logs.txt", "a+") as output_file:
+                            while True:
+                                # Splitting the read line into the output file.
+                                output_line = output_file.readline().split(", ")
+                                output_step = output_line[0]
+                                if input_step != output_step:
+                                    continue
+                                elif output_step == "1":
+                                    for element in range(len(input_line)):
+                                        if element == 0:
+                                            continue
+                                        else:
+                                            output_file.write(input_line[element] + ", ")
+                                break
+                            output_file.close()
+                        break
+                input_file.close()

e610959b98074f0185a322aab06159c06cc4a03a
WMex <52628801+wmex91@users.noreply.github.com>
2022-02-04 18:01:50 +0100

    Update causal_net.py
    Some test for adding GPU acceleration.

--- causal_net.py
+++ causal_net.py
@@ -4,25 +4,51 @@ causal_net.py: file where all the causal operations will be managed.
 This file should become a class.
 """
 
+# TODO: transfer all on Colab. Eventually retry with The new CUDA env and paddlepaddle-gpu.
+import time
 import pandas
+
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
 from causalnex.structure.notears import from_pandas
-from causalnex.plots import plot_structure, NODE_STYLE, EDGE_STYLE
+# from causalnex.plots import plot_structure, NODE_STYLE, EDGE_STYLE
+
+import networkx
+
 from IPython.display import Image
+
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 
 CSV_PATH = 'C:\\Users\\wmatt\\Desktop\\Workspace\\Projects\\Phd-Projects\\Phd-Manufacturing-Model-v3\\logs\\' \
            'merged_logs.csv'
 
+# TODO: test with GPU acceleration -> che pippa non va.
+
+# print(tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
+# print("List physical GPUs: ", tf.config.list_physical_devices('GPU'))
+
+
+print("Available GPU: " + tf.test.gpu_device_name())
+# print("Getting available GPUs: ", get_available_gpus())
+
 data = pandas.read_csv(CSV_PATH, delimiter=',')
+
 print(data.head(5))
+
+print("Starting to infer the causal net from data.")
+
+start_time = time.time()
 
 structure_model = from_pandas(data)
 
-visualize = plot_structure(
-    structure_model,
-    graph_attributes={'scale': '0.5'},
-    all_node_attributes=NODE_STYLE.WEAK,
-    all_edge_attributes=EDGE_STYLE.WEAK,
-    prog='fdp')
+print("--- %s minutes ---" % ((time.time() - start_time)/60))
 
-Image(visualize.draw(format('png')))
+print(networkx.draw(structure_model, with_labels=True))
+# Image(visualize.draw(format('png')))
+
+# networkx.drawing.nx_pydot.write_dot(structure_model, 'graph.dot')
